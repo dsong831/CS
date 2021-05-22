488b638719747a16a8f9ff43dd2d897f66bcd079
dsong <57929778+dsong831@users.noreply.github.com>
2021-05-22 18:02:06 +0900

    Update CS024

---
+++ CS019_Overflow/CS019_Overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS019_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // y = int.MaxValue + 10;
+            int x = int.MaxValue, y = 0;
+
+            checked
+            {
+                try
+                {
+                    y = x + 10;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("int.MaxValue + 10 = {0}", y);
+        }
+    }
+}
