c0cfc1ae2d49f713335c004e1a37c5a7534c303c
dsong <57929778+dsong831@users.noreply.github.com>
2021-07-10 22:19:25 +0900

    Update WFA001_MultiForms

---
+++ CS026_SplitMethod/CS026_SplitMethod_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS026_SplitMethod_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WindowsFormsApp/WFA001_MultiForms/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA001_MultiForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Click";
+            label1.Text = " ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Welcome to my project !!";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Introduce new window !!";
+
+            Form form2 = new Form2();
+            this.AddOwnedForm(form2);
+            form2.Show();
+        }
+    }
+}
